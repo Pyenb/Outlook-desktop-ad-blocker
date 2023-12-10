b2c54aede23d770391653aa5fb77cdce1124c9a0
nothing <nothing>
2023-12-11 00:40:09 +0100

    irm bugfix

--- tools/oneline.ps1
+++ tools/oneline.ps1
@@ -2,7 +2,7 @@ $batchFileUrl = "https://raw.githubusercontent.com/Pyenb/Outlook-desktop-ad-bloc
 $batchFilePath = "$env:TEMP\adblock.bat"
 
 Write-Host "Downloading batch file..."
-Invoke-WebRequest -Uri $batchFileUrl -OutFile $batchFilePath
+Invoke-RestMethod -Uri $batchFileUrl -OutFile $batchFilePath
 
 Write-Host "Executing batch file..."
 Start-Process -FilePath $batchFilePath -Wait
